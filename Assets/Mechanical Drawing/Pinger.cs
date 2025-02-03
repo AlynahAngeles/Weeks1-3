cead11bbe08adc843633c6c1d96604c7b0a7ff95
AlynahAngeles <angeleju@sheridancollege.ca>
2025-02-03 05:40:52 -0500

    Code done
    Code done; saving before writing // comments

--- Assets/Mechanical Drawing/Pinger.cs
+++ Assets/Mechanical Drawing/Pinger.cs
@@ -9,21 +9,39 @@ public class Pinger : MonoBehaviour
     [Range(0,1)]
     public float p;
 
+    public Vector3 scale;
+
     void Start()
     {
         p = 0;
+        scale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        p += Time.deltaTime;
 
-        if(p > 1)
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(mousePos.x > -5 && mousePos.x < -3 && mousePos.y > -2 && mousePos.y < 2)
         {
+
+            p += Time.deltaTime;
+
+            if(p > 1)
+            {
+
+                p = 0;
+
+            }
+
+            transform.localScale = Vector2.one * ping.Evaluate(p);
+
+        }
+
+        else{
+            transform.localScale = scale;
             p = 0;
         }
 
-        transform.localScale = Vector2.one * ping.Evaluate(p);
     }
 }
